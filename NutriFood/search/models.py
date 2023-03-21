0d33862b0a921bd725901cc583644a1a10daaf5a
selhabti <selhabti28@gmail.com>
2023-03-21 12:51:27 +0100

    database added

--- NutriFood/search/models.py
+++ NutriFood/search/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 
 
-# Create your models here.
-class produit(models.Model):
-    ingredient = models.CharField(max_length=100)
-
-
+class Produit(models.Model):
+    ingredient = models.CharField()
