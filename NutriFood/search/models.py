ce2db20e26d68db73e420107fe1d76ac8421eddc
selhabti <selhabti28@gmail.com>
2023-03-22 10:40:39 +0100

    database added

--- NutriFood/search/models.py
+++ NutriFood/search/models.py
@@ -2,4 +2,5 @@ from django.db import models
 
 
 class Produit(models.Model):
-    ingredient = models.CharField(max_length=100)
+    ingredient = models.CharField(max_length=100,null=True)
+    date_creation = models.DateTimeField(auto_now_add=True,null=True)
